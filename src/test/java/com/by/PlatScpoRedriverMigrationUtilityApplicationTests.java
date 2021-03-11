3eb60745032529dc0413e2a06cd8a60c2e34179d
Arat Rana <arat.rana@blueyonder.com>
2021-03-11 18:15:04 +0530

    Build failure fix

--- src/test/java/com/by/PlatScpoRedriverMigrationUtilityApplicationTests.java
+++ src/test/java/com/by/PlatScpoRedriverMigrationUtilityApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlatScpoRedriverMigrationUtilityApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
